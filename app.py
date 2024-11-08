df151b38d681cd468d7eef93fe4bfd6d78d446f5
hkitw <58417846+hkitw@users.noreply.github.com>
2024-11-08 08:27:48 +0800

    init-1

---
+++ app.py
@@ -0,0 +1,133 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+)
+import os
+
+app = Flask(__name__)
+
+configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
+line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@line_handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    text = event.message.text
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+
+        if text == '連絡電話':
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text="0989989095")]
+                )
+            )
+        elif text == '營業時間':
+            emojis = [
+                Emoji(index=0, product_id="5ac1bfd5040ab15980c9b435", emoji_id="001"),
+                Emoji(index=12, product_id="5ac1bfd5040ab15980c9b435", emoji_id="002")
+            ]
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text='$ 六日 PM 3~6 $', emojis=emojis)]
+                )
+            )
+        # elif text == '貼圖':
+        #     line_bot_api.reply_message(
+        #         ReplyMessageRequest(
+        #             reply_token=event.reply_token,
+        #             messages=[StickerMessage(package_id="446", sticker_id="1988")]
+        #         )
+        #     )
+        elif text == '點單':
+            url = request.url_root + '/static/list.jpg'
+            url = url.replace("http", "https")
+            app.logger.info("url=" + url)
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[
+                        ImageMessage(original_content_url=url, preview_image_url=url)
+                    ]
+                )
+            )
+        # elif text == '影片':
+        #     url = request.url_root + 'static/video.mp4'
+        #     url = url.replace("http", "https")
+        #     app.logger.info("url=" + url)
+        #     line_bot_api.reply_message(
+        #         ReplyMessageRequest(
+        #             reply_token=event.reply_token,
+        #             messages=[
+        #                 VideoMessage(original_content_url=url, preview_image_url=url)
+        #             ]
+        #         )
+        #     )
+        # elif text == '音訊':
+        #     url = request.url_root + 'static/music.mp3'
+        #     url = url.replace("http", "https")
+        #     app.logger.info("url=" + url)
+        #     duration = 60000  # in milliseconds
+        #     line_bot_api.reply_message(
+        #         ReplyMessageRequest(
+        #             reply_token=event.reply_token,
+        #             messages=[
+        #                 AudioMessage(original_content_url=url, duration=duration)
+        #             ]
+        #         )
+        #     )
+        # elif text == '位置':
+        #     line_bot_api.reply_message(
+        #         ReplyMessageRequest(
+        #             reply_token=event.reply_token,
+        #             messages=[
+        #                 LocationMessage(title='地圖', address="Kelly Gelato", latitude=24.264510239736026, longitude=120.71954415506106)
+        #             ]
+        #         )
+        #     )
+        else:
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=event.message.text)]
+                )
+            )
+
+if __name__ == "__main__":
+    app.run()
